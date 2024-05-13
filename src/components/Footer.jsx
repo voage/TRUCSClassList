f9e7a4abe059092043f890edf06d3b6962c379a3
rupokghosh <rupokghosh291@gmail.com>
2024-05-12 23:56:30 -0700

    add footer and header components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#222831]">
+      <h1 className="text-white text-md">
+        Made By{"  "}
+        <a className="text-emerald-300" href="https://github.com/rupokghosh">
+          Rupok Adin
+        </a>
+        {"  "} and{"  "}
+        <a className="text-blue-300" href="https://github.com/amrikmalhans">
+          Amrik Malhans
+        </a>
+      </h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import githubLogo from "../assets/github-mark-white.png";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center px-16 py-8 h-10 bg-[#222831]">
+      <h1 className="text-2xl text-white">TRU Computer Science Course Graph</h1>
+      <a href="https://github.com/voage/TRUCSClassList">
+        <img src={githubLogo} alt="" className="h-8 w-8" />
+      </a>
+    </div>
+  );
+};
+
+export default Header;
