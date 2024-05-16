a15f7dcd5eb862d0e0b67c74571ae6dbd720e45a
rupokghosh <rupokghosh291@gmail.com>
2024-05-15 18:25:14 -0700

    implement logic to show the nodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,62 @@
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import "./index.css";
-import ReactFlow from "reactflow";
+import ReactFlow, { Background, Controls, MiniMap } from "reactflow";
+import courseData from "./com-courses.json";
 
 import "reactflow/dist/style.css";
 
 function App() {
-  const initialNodes = [
-    { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-    { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  ];
-  const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+  console.log(courseData);
+  interface Course {
+    code: string;
+    title: string;
+    credits: string;
+    description: string;
+    prereq: string;
+  }
+
+  const transformDataToNodes = (data: Course[]): Node[] => {
+    const nodes: Node[] = [];
+    const columnCount = 7; // Number of columns in the grid
+    let x = 100; // Initial x position
+    let y = 100; // Initial y position
+
+    data.forEach((course, index) => {
+      // Calculate x and y positions based on index and columnCount
+      const columnIndex = index % columnCount;
+      const rowIndex = Math.floor(index / columnCount);
+
+      // Set position for the current node
+      const position = {
+        x: x + columnIndex * 300, // Adjust the spacing between columns as needed
+        y: y + rowIndex * 200, // Adjust the spacing between rows as needed
+      };
+
+      // Create node and push to nodes array
+      const node: Node = {
+        id: course.code,
+        type: "default",
+        data: { label: course.code },
+        position,
+        dragHandle: false,
+        dragging: false,
+      };
+
+      nodes.push(node);
+    });
+
+    return nodes;
+  };
+
+  const nodes: Node[] = transformDataToNodes(courseData);
   return (
     <div style={{ fontFamily: "'Poppins', sans-serif " }}>
       <Header />
-      <div style={{ width: "80vw", height: "80vh" }}>
-        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      <div style={{ width: "100vw", height: "200vh" }}>
+        <ReactFlow nodes={nodes} fitView>
+          <Controls />
+        </ReactFlow>
       </div>
       <Footer />
     </div>
